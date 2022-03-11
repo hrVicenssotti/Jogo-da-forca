603c3ff538c97857be34defc6fafae22182851bf
RawsenDead <horlancar.8@gmail.com>
2022-03-11 15:00:20 -0400

    Logica contruction

--- main.js
+++ main.js
@@ -1,61 +1,69 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
+function criarBotao(letra) {
+    const botao = document.createElement('button')
+    botao.value = letra
+    botao.innerHTML = letra
+    return botao
+}
 function criarBotoes() {
     const buttons = document.querySelector('#buttonsLetras')
     const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    function criarBotao(letra) {
-        const botao = document.createElement('button')
-        const l = letra.toUpperCase()
-        botao.value = l
-        botao.innerHTML = l
-        return botao
-    }
     for (let i in letras) {
         const botao = criarBotao(letras[i])
         buttons.appendChild(botao)
     }
-    document.querySelectorAll('.buttons>button').forEach(button => {
-        button.onclick = function(click) {
-            console.log(click.target)      
-        }
+}
+document.querySelectorAll('.buttons>button').forEach(button => {
+    button.onclick = function(click) {
+        console.log(click.target)      
+    }
+})
+const draw = {
+    line: function(props) {
+        props.context.strokeStyle = props.cor
+        props.context.beginPath()
+        props.context.moveTo(props.xx, props.xy)
+        props.context.lineTo(props.yx, props.yy)
+        props.context.lineCap = props.lineCap
+        props.context.lineWidth = props.larguraLinha
+        props.context.stroke()
+    },
+    rect: function(props) {
+        props.context.fillStyle = props.cor
+        props.context.beginPath()
+        props.context.fillRect(props.x, props.y, props.w, props.h)
+    },
+    circle: function(props) {
+        props.context.strokeStyle = props.cor
+        props.context.beginPath()
+        props.context.arc(props.x, props.y, props.r, 0, 2 * Math.PI)
+        props.context.lineWidth = props.larguraLinha
+        props.context.stroke()
+    }
+}
+function loadArray(array) {
+    array.forEach(item => {
+        const run = draw[item.tipo]
+        run(item)
     })
-}
-function drawRect(context, x, y, width, heigth, cor) {
-    context.fillStyle = cor
-    context.beginPath()
-    context.fillRect(x, y, width, heigth)
-}
-function drawCircle(context, x, y, raio, cor, larguraLinha) {
-    context.strokeStyle = cor
-    context.beginPath()
-    context.arc(x, y, raio, 0, 2 * Math.PI)
-    context.lineWidth = larguraLinha
-    context.stroke()
-}
-function drawLine(context, xx, xy, yx, yy, cor, larguraLinha, lineCap = 'butt') {
-    context.strokeStyle = cor
-    context.beginPath()
-    context.moveTo(xx, xy)
-    context.lineTo(yx, yy)
-    context.lineCap = lineCap
-    context.lineWidth = larguraLinha
-    context.stroke()
 }
 function criarForca(context) {
     const cor = 'black'
-    drawRect(context, 60, 50, 10, 300, cor)
-    drawRect(context, 60, 50, 140, 10, cor)
-    drawRect(context, 20, 350, 90, 10, cor)
-    drawRect(context, 200, 50, 10, 20, cor)
+    const forca = [
+        {context, tipo: 'rect', x: 60, y: 50, w: 10, h: 300, cor}, {context, tipo: 'rect', x: 60, y: 50, w: 140, h: 10, cor},
+        {context, tipo: 'rect', x: 20, y: 350, w: 90, h: 10, cor}, {context, tipo: 'rect', x: 200, y: 50, w: 10, h: 20, cor}
+    ]
+    loadArray(forca)
 }
 function criarBoneco() {
     const corBoneco = 'black'
-    drawCircle(context, 205, 100, 30, corBoneco, 3  )
-    drawLine(context, 205, 132, 205, 230, corBoneco, 5, 'round')
-    drawLine(context, 170, 180, 203, 140, corBoneco, 5, 'round')
-    drawLine(context, 243, 180, 207, 140, corBoneco, 5, 'round')
-    drawLine(context, 230, 300, 205, 230, corBoneco, 5, 'round')
-    drawLine(context, 180, 300, 205, 230, corBoneco, 5, 'round')
+    const lineWidth = 5
+    const boneco = [
+        {tipo: 'circle', x: 205, y: 100, r: 30}, {tipo: 'line', xx: 205, xy: 132, yx: 205, yy: 230}, {tipo: 'line', xx: 170, xy: 180, yx: 203, yy: 140}, 
+        {tipo: 'line', xx: 243, xy: 180, yx: 207, yy: 140}, {tipo: 'line', xx: 230, xy: 300, yx: 205, yy: 230}, {tipo: 'line', xx: 180, xy: 300, yx: 205, yy: 230}
+    ]
+    // loadArray(boneco)
 }
 criarBoneco()
 criarForca(context)
